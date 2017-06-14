61336a7af8f765c5fced906b0066ff7bf84f81bb
Vincent WUHRLIN <vincent.wuhrlin@coursierprive.com>
2017-06-14 13:09:04 +0200

    Formation

---
+++ corrections/02 - Le langage Javascript/js/app.js
@@ -0,0 +1,139 @@
+
+/*
+ * Classe et constructeur
+ */
+var Application = function()
+{
+    this.name = "ORSYS Tunes";
+    this.version = "v1.0";
+    this.initialized = false;
+    this.songs =
+    [
+      {title: "New Year's Day", artist: "U2"},
+      {title: "Highway to Hell", artist: "AC/DC"},
+      {title: "Beat It", artist: "Michael Jackson"},
+      {title: "Smoke on the Water", artist: "Deep Purple"},
+    ];
+}
+
+/*
+ * Méthode de lancement
+ */
+Application.prototype.start = function()
+{
+    // Lancement
+    console.info("Starting " + this.name + " " + this.version + "...");
+    console.debug(this.name + " " + this.version + " started at " + new Date() + " !");
+
+    // Ajout d'une musique dans start
+    this.songs.push({title: "Light My Fire", artist: "The Doors"});
+
+    // Application initialisée
+    this.initialized = true;
+    if (this.initialized)
+    {
+        console.debug(this.name + " " + this.version + " started sucessfully !");
+    }
+}
+
+/*
+ * Méthode de debug (affiche les musiques en mémoire)
+ */
+Application.prototype.debug = function()
+{
+    console.debug("******************** DEBUG ********************");
+    console.debug("Songs (" + this.songs.length + ") :");
+    for (var i = 0; i < this.songs.length; i++)
+    {
+        console.debug("- " + this.songs[i].title + " / " + this.songs[i].artist); 
+    }
+    console.debug("***********************************************");
+}
+
+/*
+ * Méthode globale d'initialisation
+ */
+function initialize()
+{   
+    try
+    {
+        console.debug("Initialize !");
+
+        // Créer une application
+        var application = new Application();
+
+        // Démarre l'application
+        application.start(); 
+
+        // Debug l'application
+        application.debug();
+    }
+    catch(e)
+    {
+        console.error("Error when initializing application", e);
+    }
+}
+
+/*
+* Exercice 2.6.1
+*/
+function createArray()
+{  
+    // Initialisation de l'array
+    var array = [];
+
+    // Ajout de 10000 éléments
+    for (var i = 1; i <= 10000; i++)
+    {
+        var index = "00000";
+        var position = new Number(i).toString();
+        var element = "Element " + index.substring(0, (index.length - position.length)) + position;
+        array.push(element);
+    }
+
+    // Suppression des éléments
+    array.splice(2, 9996);
+
+    // Transformation de l'array en string
+    var result = array.join(", ");
+
+    // Affichage du résultat
+    console.info("******************** Exerice 2.6.1 ********************");
+    console.info(result);
+    console.info("*******************************************************");
+}
+
+/*
+ * Exercice 2.6.2
+ */
+
+// Déclaration des mois (global)
+var MONTHS = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+
+// On ajoute une méthode "getCurrentMonthInFrench" dans la classe native "Date" de Javascript
+// et on fait pointer vers notre méthode globale "getCurrentMonthInFrench"
+Date.prototype.getCurrentMonthInFrench = getCurrentMonthInFrench;
+function getCurrentMonthInFrench()
+{
+    var month = this.getMonth();
+    return MONTHS[month];
+}
+
+// Méthode de test
+function customizeDate()
+{
+    var date = new Date();
+    console.info("******************** Exerice 2.6.2 ********************");
+    console.info(date.getCurrentMonthInFrench());
+    console.info("*******************************************************");
+}
+
+// Créer une application et l'initialize
+initialize();
+
+// Exercice 2.6.1
+createArray();
+
+// Exercice 2.6.2
+customizeDate();
+
---
+++ corrections/05 - Interaction avec les feuilles de style en cascade/js/app.js
@@ -0,0 +1,166 @@
+
+/*
+* Classe et constructeur
+*/
+var Application = function()
+{
+    this.name = "ORSYS Tunes";
+    this.version = "v1.0";
+    this.initialized = false;
+    this.songs =
+    [
+      {title: "New Year's Day", artist: "U2"},
+      {title: "Highway to Hell", artist: "AC/DC"},
+      {title: "Beat It", artist: "Michael Jackson"},
+      {title: "Smoke on the Water", artist: "Deep Purple"},
+    ];
+}
+
+/*
+* Méthode de lancement
+*/
+Application.prototype.start = function()
+{
+    // Lancement
+    console.info("Starting " + this.name + " " + this.version + "...");
+    console.debug(this.name + " " + this.version + " started at " + new Date() + " !");
+    document.title = this.name + " " + this.version;
+
+    // Ajout d'une musique dans start
+    this.songs.push({title: "Light My Fire", artist: "The Doors"});
+
+    // Bouton login (seulement sur la page index.html)
+    var loginButton = document.getElementById("loginButton");
+    if (loginButton != null)
+    {
+        loginButton.addEventListener("click", this.onLogin);
+    }
+
+    // Anime le pannel de connexion
+    var connectionPanel = document.getElementById("connectionPanel");    
+    if (connectionPanel != null)
+    {
+        connectionPanel.style.backgroundColor = "#009688";
+        setTimeout(function()
+        {
+            connectionPanel.classList.add("visible");
+        }, 100);
+        setTimeout(function()
+        {
+            connectionPanel.style.backgroundColor = "#607d8b";
+        }, 1000);
+    }
+
+    // Application initialisée
+    this.initialized = true;
+    if (this.initialized)
+    {
+        console.debug(this.name + " " + this.version + " started sucessfully !");
+    }
+}
+
+/*
+* Méthode de debug (affiche les musiques en mémoire)
+*/
+Application.prototype.debug = function()
+{
+    console.debug("******************** DEBUG ********************");
+    console.debug("Songs (" + this.songs.length + ") :");
+    for (var i = 0; i < this.songs.length; i++)
+    {
+        console.debug("- " + this.songs[i].title + " / " + this.songs[i].artist); 
+    }
+    console.debug("***********************************************");
+}
+
+/*
+* Méthode de connexion (login)
+*/
+Application.prototype.onLogin = function(event)
+{
+    // Verification de l'email
+    var email = document.getElementById("email");
+    var emailError = document.getElementById("emailError");
+    if (email.validity.valueMissing)
+    {
+        event.preventDefault();
+        emailError.textContent = "Email manquant";
+        email.classList.add("error");
+    }
+    else if (!/^[\w-]+@([\w-]+\.)+[\w-]+$/.test(email.value))
+    {
+        event.preventDefault();
+        emailError.textContent = "Email invalide";
+        email.classList.add("error");
+    }
+    else
+    {
+        emailError.textContent = "";
+        email.classList.remove("error");
+    }
+
+    // Verification du mot de passe
+    var password = document.getElementById("password");
+    var passwordError = document.getElementById("passwordError");
+    if (password.validity.valueMissing)
+    {
+        event.preventDefault();
+        passwordError.textContent = "Mot de passe manquant";
+        password.classList.add("error");
+    }
+    else if (!/^.{4,8}$/.test(password.value))
+    {
+        event.preventDefault();
+        passwordError.textContent = "Mot de passe invalide";
+        password.classList.add("error");
+    }
+    else
+    {
+        passwordError.textContent = "";
+        password.classList.remove("error");
+    }
+
+    // Si aucune erreur de validation
+    if ((passwordError.textContent == "") && (emailError.textContent == ""))
+    {
+        // Verification du compte et redirection
+        var credentialsError = document.getElementById("credentialsError");
+        if ((email.value == "admin@test.com") && (password.value == "1234"))
+        {
+            window.location = "app.html";
+        }
+
+        // Erreur
+        else
+        {
+            credentialsError.textContent = "Informations de connexion invalides";
+            event.preventDefault();  
+        }
+    }
+}
+
+/*
+* Méthode globale d'initialisation
+*/
+function initialize()
+{   
+    try
+    {
+        console.debug("Initialize !");
+
+        // Créer une application
+        var application = new Application();
+
+        // Démarre l'application
+        application.start(); 
+
+        // Debug l'application
+        application.debug();
+    }
+    catch(e)
+    {
+        console.error("Error when initializing application", e);
+    }
+}
+
+initialize();
